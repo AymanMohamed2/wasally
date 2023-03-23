24d82ea12bec04c3beb1f8a86a3e07538a7acd20
darknaya.am@gmail.com <darknaya.am@gmail.com>
2023-03-23 02:20:14 +0200

    add file strings to core folder

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'core/utils/app_strings.dart';
+import 'features/splash/presentation/view/splash_view.dart';
+
+class Wasally extends StatelessWidget {
+  const Wasally({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: AppStrings.appName,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import 'features/splash/presentation/view/splash_view.dart';
+import 'package:wasally/app.dart';
 
 void main() {
   runApp(const Wasally());
-}
-
-class Wasally extends StatelessWidget {
-  const Wasally({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
-    );
-  }
 }
