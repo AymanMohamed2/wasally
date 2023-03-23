062a5a8d32de72c6ae497edecc36d6a2e8106738
darknaya.am@gmail.com <darknaya.am@gmail.com>
2023-03-23 02:27:00 +0200

    add colors file to core folderand change primary color

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'core/utils/app_colors.dart';
 import 'core/utils/app_strings.dart';
 import 'features/splash/presentation/view/splash_view.dart';
 
@@ -12,6 +13,7 @@ class Wasally extends StatelessWidget {
     return GetMaterialApp(
       title: AppStrings.appName,
       theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
           iconTheme: IconThemeData(color: Colors.black),
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.orange;
+  static const Color hint = Colors.grey;
+}
